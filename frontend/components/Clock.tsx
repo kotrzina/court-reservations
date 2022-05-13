642120e8468aae8e3b0262f056483c1de8e8f5c0
Tomas Kozak <kozak@talko.cz>
2022-05-13 17:58:28 +0200

    Fix clock

--- frontend/components/Clock.tsx
+++ frontend/components/Clock.tsx
@@ -6,16 +6,18 @@ type Props = {
 export const Clock: FC<Props> = (props: Props) => {
 
     function convertUnicode(): string {
+        let halfDelta = props.slot % 2 === 1 ? 12 : 0
+
         let s = props.slot / 2
-        if (s > 12) {
+        if (s > 13) {
             s -= 12
-        } else if (s === 0 || s === 12) {
+        } else if ([0, 1, 12, 13].indexOf(s) > -1) {
+            // 12:00 is at the end of array :shrug:
             s = 12
         }
 
-        const halfDelta = props.slot % 2 === 1 ? 12 : 0
-        const code = 128335 + Math.floor(s) + halfDelta
 
+        const code = 128335 + Math.floor(s) + halfDelta
         return "&#" + code.toString() + ";"
     }
 
